0c5cc77464850cade105efaf7b850707ef3d8fe4
valnicky <valery0007@gmail.com>
2021-08-21 14:13:32 +0200

    change styles css

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 init();
 
-
-
-
 function init() {
     const changelightbleu = document.querySelector('.light-bleu.square');
     const changedarkbleu = document.querySelector('.dark-bleu.square');
-    const changebleu = document.querySelector('.bleu.square');
+    const changebleugris = document.querySelector('.bleugris.square');
+    const changegreen = document.querySelector('.green.square');
+
+
     const lightbleu = "#02b3e4";
     const darkbleu = "#2d3c49";
     const bleugris = "#7d97ad";
-
+    const green = "#235300";
     const selecttexth1 = document.querySelector('.container .header .texto h1');
     const selecttexth3 = document.querySelector('.container .header .texto h3');
-    const selecthr = document.querySelector('.container .main hr');
+    const selecthr = document.querySelector('.container .container__center .main hr');
+    const borderimg = document.querySelector('.header__img img');
 
     changelightbleu.addEventListener('click', () => { changecolor(lightbleu) });
     changedarkbleu.addEventListener('click', () => { changecolor(darkbleu) });
-    changebleu.addEventListener('click', () => { changecolor(bleugris) });
+    changebleugris.addEventListener('click', () => { changecolor(bleugris) });
+    changegreen.addEventListener('click', () => { changecolor(green) });
 
     function changecolor(color) {
 
         selecttexth1.style.color = color;
         selecttexth3.style.color = color;
         selecthr.style.backgroundColor = color;
-        console.log(color);
+        borderimg.style.border = '4px solid' + color;
+        // console.log(color);
     }
 
 
