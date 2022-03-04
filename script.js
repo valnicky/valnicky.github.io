d65095679653022c6d1e4f5121f9975f7e94b945
valnicky <valery0007@gmail.com>
2022-03-04 17:02:36 +0100

    cnge color for accesibility

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function init() {
     const changereddark = document.querySelector('.red-dark.square');
     const changegreen = document.querySelector('.green.square');
 
-    const lightbleu = "#02b3e4";
+    const lightbleu = "#0aa6d1";
     const darkbleu = "#2d3c49";
     const reddark = "#b3311c";
     const green = "#235300";
